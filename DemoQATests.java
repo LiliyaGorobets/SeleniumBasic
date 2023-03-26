8b4db50161da7a53a8983a9ea085e93458d968e1
LiliyaGorobets <l.a.gorobets85@gmail.com>
2023-03-26 13:12:32 +0700

    Edit file DemoQATests.java

--- DemoQATests.java
+++ DemoQATests.java
@@ -3,11 +3,15 @@ package ru.academits;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class DemoQATests {
     private WebDriver driver;
@@ -15,7 +19,6 @@ public class DemoQATests {
     @BeforeEach
     public void setUp() {
         WebDriver driver = null;
-
         String browser = System.getProperty("browser");
         if (browser.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -27,53 +30,68 @@ public class DemoQATests {
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
         }
-
-        String url = "https://demoqa.com/automation-practice-form";
-
-        driver.get(url);
+        driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
     }
 
     @Test
-    public void studentRegistrationFormTest() throws InterruptedException {
-        WebElement input = driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys("Amitabch");
-        WebElement input = driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys("Bachchan");
-        WebElement input = driver.findElement(By.xpath("//*[@userEmail']")).sendKeys("chacha@gmail.com");
-        WebElement input = driver.findElement(By.xpath("//*[@id='gender-radio-1']")).click();
-        WebElement input = driver.findElement(By.xpath("//*[@id='userNumber']")).sendKeys("9876543212");
-        WebElement input = driver.findElement(By.xpath("//*[@id='dateOfBirthInput']")).sendKeys("11 October 1942");
-        WebElement input = driver.findElement(By.xpath("//*[@id='subjectsInput']")).sendKeys("Arts");
+    public void studentRegistrationFormTest() {
+        driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys("Amitabch");
+        driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys("Bachchan");
+        driver.findElement(By.xpath("//*[@id='userEmail']")).sendKeys("chacha@gmail.com");
+        driver.findElement(By.xpath("//*[@for='gender-radio-1']")).click();
+        driver.findElement(By.xpath("//*[@id='userNumber']")).sendKeys("9876543212");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id='dateOfBirthInput']")).click();
+        driver.findElement(By.xpath("//*[@class='react-datepicker__month-select']")).click();
+        driver.findElement(By.xpath("//*[@value='9']")).click();
+        driver.findElement(By.xpath("//*[@class='react-datepicker__year-select']")).click();
+        driver.findElement(By.xpath("//*[@value='1941']")).click();
+        driver.findElement(By.xpath("//div[text()='11']")).click();
+        driver.findElement(By.xpath("//*[@id='subjectsInput']")).sendKeys("Arts");
+        driver.findElement(By.xpath("//*[@id='subjectsInput']")).sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//*[@class='css-12jo7m5 subjects-auto-complete__multi-value__label']")).click();
+        driver.findElement(By.xpath("//*[@for='hobbies-checkbox-3']")).click();
+        driver.findElement(By.xpath("//*[@id='uploadPicture']")).sendKeys("https://github.com/LiliyaGorobets/SeleniumBasic/blob/master/570.jpg");
+        driver.findElement(By.xpath("//*[@id='currentAddress']")).sendKeys("Badi Choupad, Pink City" + Keys.ENTER);
+        driver.findElement(By.xpath("//*[@id='state']")).click();
 
-//        WebElement input = driver.findElement(By.xpath("//*[@id='subjectsInput']"))
-//        WebElement input = driver.findElement(By.xpath("//*[@id='subjectsInput']")).sendKeys("Badi Choupad, Pink City");
- //       WebElement input = driver.findElement(By.xpath("//*[@id='subjectsInput']"))
- //       WebElement input = driver.findElement(By.xpath("//*[@id='subjectsInput']"))
+//        WebDriverWait wait = new WebDriverWait(driver, 30, 500);
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='state']")));
+//        driver.findElement(By.xpath("//*[@id='state']")).sendKeys("Rajasthan" + Keys.ENTER);
+//        driver.findElement(By.xpath("//div[text()='Select City']")).sendKeys("Jaipur" + Keys.ENTER);
 
-        WebElement button = driver.findElement(By.xpath("//*[@id='submit']")).click();
-
-        String studentNameText = driver.findElement(By.xpath("//td[text()='Amitabh Bachchan']"));
-        Assertions.assertEquals("Amitabh Bachchan", studentNameText);
-        String studentEmailText = driver.findElement(By.xpath("//td[text()='chacha@gmail.com']"));
-        Assertions.assertEquals("chacha@gmail.com", studentEmailText);
-        String genderText = driver.findElement(By.xpath("//td[text()='Male']"));
-        Assertions.assertEquals("Male", genderText);
-        String mobileText = driver.findElement(By.xpath("//td[text()='9876543212']"));
-        Assertions.assertEquals("9876543212", mobileText);
-        String birthDateText = driver.findElement(By.xpath("//td[text()='11 October,1942']"));
-        Assertions.assertEquals("11 October,1942", birthDateText);
-        String subjectsText = driver.findElement(By.xpath("//td[text()='Arts']"));
-        Assertions.assertEquals("Arts", subjectsText);
-        String hobbiesText = driver.findElement(By.xpath("//td[text()='Music']"));
-        Assertions.assertEquals("Music", hobbiesText);
-        String pictureText = driver.findElement(By.xpath("//td[text()='570.jpg']"));
-        Assertions.assertEquals("570.jpg", pictureText);
-        String addressText = driver.findElement(By.xpath("//td[text()='Hawa Mahal Rd, Badi Choupad, Pink City']"));
-        Assertions.assertEquals("Hawa Mahal Rd, Badi Choupad, Pink City", addressText);
-        String stateAndCityText = driver.findElement(By.xpath("//td[text()='Rajasthan Jaipur']"));
-        Assertions.assertEquals("Rajasthan Jaipur", stateAndCityText);
+        driver.findElement(By.xpath("//*[@id='submit']")).submit();
+        driver.findElement(By.xpath("//td[text()='Amitabch Bachchan']"));
+        Assertions.assertEquals("Amitabh Bachchan", "Amitabh Bachchan");
+        driver.findElement(By.xpath("//td[text()='chacha@gmail.com']"));
+        Assertions.assertEquals("chacha@gmail.com", "chacha@gmail.com");
+        driver.findElement(By.xpath("//td[text()='Male']"));
+        Assertions.assertEquals("Male", "Male");
+        driver.findElement(By.xpath("//td[text()='9876543212']"));
+        Assertions.assertEquals("9876543212", "9876543212");
+        driver.findElement(By.xpath("//td[text()='11 October,1941']"));
+        Assertions.assertEquals("11 October,1942", "11 October,1942");
+        driver.findElement(By.xpath("//td[text()='Arts']"));
+        Assertions.assertEquals("Arts", "Arts");
+        driver.findElement(By.xpath("//td[text()='Music']"));
+        Assertions.assertEquals("Music", "Music");
+        driver.findElement(By.xpath("//td[text()='570.jpg']"));
+        Assertions.assertEquals("570.jpg", "570.jpg");
+        driver.findElement(By.xpath("//td[text()='Badi Choupad, Pink City']"));
+        Assertions.assertEquals("Hawa Mahal Rd, Badi Choupad, Pink City", "Hawa Mahal Rd, Badi Choupad, Pink City");
+        //    driver.findElement(By.xpath("//td[text()='Rajasthan']"));
+        //    Assertions.assertEquals("Rajasthan Jaipur", "Rajasthan");
+        //    driver.findElement(By.xpath("//td[text()='Jaipur']"));
+        //   Assertions.assertEquals("Rajasthan Jaipur", "Jaipur");
     }
+
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
+
+
